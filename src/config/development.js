6beca47629c2826d950fb667f5bb652936cb56a1
qlanoe <lanoe-q@mgdis.fr>
2018-08-14 09:16:47 +0200

    fix(modif xav): ajout de modification

--- src/config/development.js
+++ src/config/development.js
@@ -1,5 +1,6 @@
 const config = {
   LOG_LEVEL: process.env.LOG_LEVEL || "debug",
   SWAGGER_ACTIVATED: process.env.SWAGGER_ACTIVATED || true,
+  MONGO_URL: process.env.MONGO_URL || '/192.168.136.128:27017',
 };
 module.exports = { config: config };
